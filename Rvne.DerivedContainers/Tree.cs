08b922d29f07646ba0646196825562c7c0dfbe97
RoyPrinsGH <royprinsgm@gmail.com>
2026-02-10 21:04:47 +0100

    Add a map function to Tree

--- Rvne.DerivedContainers/Tree.cs
+++ Rvne.DerivedContainers/Tree.cs
@@ -24,7 +24,7 @@ public class Tree<T>
     /// <param name="methodName">The instance method name to invoke on each <typeparamref name="T" /> value.</param>
     /// <param name="args">Arguments passed to each method invocation.</param>
     /// <returns>A tree with the same structure whose values are the invocation results cast to <typeparamref name="R" />.</returns>
-    public Tree<R> CallMethod<R>(string methodName, params object[] args)
+    protected Tree<R> CallNativeMethod<R>(string methodName, params object[] args)
     {
         ArgumentNullException.ThrowIfNull(methodName);
 
@@ -53,6 +53,35 @@ public class Tree<T>
             if (node.Children is { Count: > 0 } children)
             {
                 result.Children = [.. children.Select(child => CallMethodCore(child, method, args))];
+            }
+
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// Projects each node value in the tree using <paramref name="map" /> and returns a tree with the same structure.
+    /// </summary>
+    /// <param name="map">Projection function applied to each <typeparamref name="T" /> value.</param>
+    /// <returns>A tree containing projected values of type <typeparamref name="R" />.</returns>
+    protected Tree<R> Map<R>(Func<T, R> map)
+    {
+        ArgumentNullException.ThrowIfNull(map);
+
+        return MapCore(this, map);
+
+        static Tree<R> MapCore(Tree<T> node, Func<T, R> map)
+        {
+            var result = new Tree<R>();
+
+            if (node.Value is { } value)
+            {
+                result.Value = map(value);
+            }
+
+            if (node.Children is { Count: > 0 } children)
+            {
+                result.Children = [.. children.Select(child => MapCore(child, map))];
             }
 
             return result;
